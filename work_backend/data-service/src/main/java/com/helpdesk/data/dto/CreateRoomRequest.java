f67d34cbfbd62863b2a3ecfe6990fd9369ddc301
siyamraihan-work <siyamraihan.work@gmail.com>
2026-04-15 16:20:56 -0700

    file renamed

--- work_backend/data-service/src/main/java/com/helpdesk/data/dto/CreateRoomRequest.java
+++
@@ -1,9 +0,0 @@
-package com.helpdesk.data.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record CreateRoomRequest(
-        @NotNull Long buildingId,
-        @NotBlank String name
-) {}
